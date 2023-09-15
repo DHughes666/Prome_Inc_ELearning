1d806e7d8ccca3295ef0ca0e8e0505d6c1a67506
DHughes666 <graziemolto6@gmail.com>
2023-09-15 17:05:03 +0100

    Formsets etc ...So far so good

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from . import views
 
-from .views import home
 
 urlpatterns = [
-    path('home', home, name='home'),
+    path('mine/', views.ManageCourseListView.as_view(),
+         name='manage_course_list'), 
+    path('create/', views.CourseCreateView.as_view(),
+         name='course_create'),
+    path('<pk>/edit/', views.CourseUpdateView.as_view(),
+         name='course_edit'),
+    path('<pk>/delete/', views.CourseDeleteView.as_view(),
+         name='course_delete'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(),
+         name='course_module_update'),
 ]
