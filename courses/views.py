7febc7b0b25bdba219af8eba79657163a5f89c6d
DHughes666 <graziemolto6@gmail.com>
2023-09-16 21:50:09 +0100

    Drag n Drop using HTML5

--- courses/views.py
+++ courses/views.py
@@ -15,6 +15,8 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
 
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
 
 # Create your views here.
 class OwnerMixin:
@@ -161,4 +163,22 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+    
+
+class ModuleOrderView(CsrfExemptMixin,
+                      JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+
+class ContentOrderView(CsrfExemptMixin, 
+                       JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
 
